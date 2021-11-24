5282c890390e632e737e14838c940d494833c910
Presley Acuna <presleyacuna@gmail.com>
2021-11-24 07:53:00 -0500

    Added print statements to help debug fromFIle2()

--- AsciiArt.py
+++ AsciiArt.py
@@ -4,15 +4,23 @@
 class AsciiArt:
     def __init__(self, characters):
         self._characters = characters
+        print("init print\n", self._characters)
 
     @classmethod
     def fromFile(cls, filename):
         with open(filename) as fileObj:
             characters = fileObj.read()
+            print("fromFile print\n", characters)
             return cls(characters)
 
+    def fromFile2(self, filename):
+        with open(filename) as fileObj:
+            characters = fileObj.read()
+            print("fromFile2 print\n", self._characters)
+            return (characters)
+
     def display(self):
-        print(self._characters)
+        print("display print\n", self._characters)
 
     # Other AsciiArt methods wold go here....
 
@@ -26,3 +34,5 @@ face1.display()
 face2 = AsciiArt.fromFile('face.txt')
 face2.display()
 
+face3 = AsciiArt.fromFile2('face.txt')
+face3.display()
